35374b33b11b2c80596343a86494f33aa06eb190
AMIT KUMAR <113933332+amitraj99053@users.noreply.github.com>
2025-01-15 21:24:35 +0530

    Add files via upload

---
+++ Malloc1.c
@@ -0,0 +1,36 @@
+/*C program to create memory for int, 
+char and float variable at run time.*/
+ 
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main()
+{
+    int *iVar;
+    char *cVar;
+    float *fVar;
+     
+    // allocating memory dynamically
+     
+    iVar=(int*)malloc(1*sizeof(int));
+    cVar=(char*)malloc(1*sizeof(char));
+    fVar=(float*)malloc(1*sizeof(float));
+     
+    printf("Enter integer value: ");
+    scanf("%d",iVar);
+     
+    printf("Enter character value: ");
+    scanf(" %c",cVar);
+     
+    printf("Enter float value: ");
+    scanf("%f",fVar);
+     
+    printf("Inputted value are: %d, %c, %.2f\n",*iVar,*cVar,*fVar);
+     
+    free(iVar);
+    free(cVar);
+    free(fVar);
+ 
+    return 0;
+}
+
---
+++ Malloc2.c
@@ -0,0 +1,29 @@
+/*C program to input and print text 
+using Dynamic Memory Allocation.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    char *text;
+    
+    printf("Enter limit of the text: ");
+    scanf("%d",&n);
+    
+    //allocate memory dynamically
+    text=(char*)malloc(n*sizeof(char));
+    
+    printf("Enter text: ");
+    scanf(" "); //clear input buffer
+    gets(text);
+    
+    printf("Inputted text is: %s\n",text);
+    
+    // Free Memory
+    free(text);
+    
+    return 0;
+}
+
