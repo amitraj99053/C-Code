a4e6efb89e1e564e14836d6fda06c87394873c5f
AMIT KUMAR <113933332+amitraj99053@users.noreply.github.com>
2025-01-23 23:03:23 +0530

    Add files via upload

---
+++ CalendarTime.c
@@ -0,0 +1,14 @@
+//  show the first of calendar time
+
+#include <stdio.h>
+#include <time.h>
+
+int main(void)
+{
+    struct tm first = {.tm_mday=1};
+
+    mktime(&first);
+
+    printf("\n%s\n", asctime(&first));
+}
+
