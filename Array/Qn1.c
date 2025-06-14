c7577654f019ee832a351afa684123dd33dd05a7
Amit Kumar <amitraj99053@gmail.com>
2025-06-14 23:38:31 +0530

    Upload

---
+++ Array/Qn1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int arr[5] = {2, 4, 8, 12, 16};
+
+    printf("%d ", arr[2]);
+
+    printf("%d ", arr[4]);
+
+    printf("%d ", arr[0]);
+    return 0;
+}
